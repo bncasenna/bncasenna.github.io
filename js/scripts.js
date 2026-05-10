5de798db46058f4ad2bcb86f8e4bfddbebc67192
bncasenna <biancasenna@outlook.com.br>
2026-05-10 19:45:12 -0300

    Adicionar paginas e atualizar projeto.

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,30 @@ function trocarIdioma(idioma) {
     }
 }
 trocarIdioma('pt');
+
+const imagens = document.querySelectorAll('.gallery img'); 
+const modal = document.getElementById('modal');
+const modalImg = document.querySelector('#modal img');
+const closeImg = document.querySelector('.btnClose');
+
+imagens.forEach(imagem =>{
+    imagem.addEventListener('click', () =>{
+        const imgSelect = imagem.src;
+        modalImg.src = imgSelect;
+        
+        modal.style.display = 'flex';
+        closeImg.style.display = 'block';
+    })
+});
+
+closeImg.addEventListener('click', ()=>{
+        closeImg.style.display = 'none';
+        modal.style.display = 'none';
+    })
+
+modal.addEventListener('click', (event)=>{
+    if(event.target === modal){
+        modal.style.display = 'none';
+        closeImg.style.display = 'none';
+    }
+})
